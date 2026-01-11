9cdd0a64b11de7a64e2d0237aff377c2bbeaa20b
Yasarasenevirathne <164550676+Yasarasenevirathne@users.noreply.github.com>
2026-01-11 12:52:34 +0530

    Finished frontend section modifications

--- frontend/src/components/FoodDisplay/FoodDispaly.jsx
+++ frontend/src/components/FoodDisplay/FoodDispaly.jsx
@@ -3,16 +3,14 @@ import './FoodDisplay.css'
 import { StoreContext } from '../../context/StoreContext'
 import FoodItem from '../FoodItem/FoodItem'
 
-const FoodDisplay = ({category}) => {
-
-
+const FoodDispaly = ({category}) => {
 
 
   const {food_list} = useContext(StoreContext)
 
   return (
     <div className='food-display' id='food-display'>
-      <h2>Top dishes near you...</h2>
+      <h2>Top dishes near you</h2>
       <div className="food-display-list">
         {food_list.map((item,index)=>{
           if (category==="All" || category===item.category){
@@ -26,4 +24,4 @@ const FoodDisplay = ({category}) => {
   )
 }
 
-export default FoodDisplay
+export default FoodDispaly
